44a9ffa1332358f62d795c1e6d1f949710ebca7f
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2026-02-22 15:51:18 +0530

    frontend setup done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div>
+      <h1 className='text-5xl font-bold text-blue-200'>Hello world</h1>
       this is investigo app
     </div>
   )
