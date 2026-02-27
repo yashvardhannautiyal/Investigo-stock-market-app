8e9a050039ae887d8b91be0d2a117649556c9052
yashvardhannautiyal <yashvardhannautiyal@gmail.com>
2026-02-27 19:58:58 +0530

    not found page added

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function NotFound() {
   return (
-    <div>
-      this is not found page
+    <div className='text-center p-12 my-24'>
+      <p className='font-extrabold text-9xl'>404</p>
+      <p className='font-bold text-2xl'>Not Found</p>
+      <p>Page you are looking for can't be found</p>
     </div>
   )
 }
